ab1da8362be5da3f7cc0633552873daf218712a3
gyeiv <78514824+gyeiv@users.noreply.github.com>
2022-05-27 15:35:40 -0700

    Initial program setup.

---
+++ programs/hh-escrow/src/lib.rs
@@ -0,0 +1,15 @@
+use anchor_lang::prelude::*;
+use solana_program::entrypoint::ProgramResult;
+
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+#[program]
+pub mod hh_escrow {
+    use super::*;
+    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
