88552d74f58323e490240c95ae1c050e0b8fe477
gyeiv <78514824+gyeiv@users.noreply.github.com>
2022-05-31 14:19:06 -0700

    fix some formatting

--- tests/custom-matchers.d.ts
+++ tests/custom-matchers.d.ts
@@ -1,21 +1,21 @@
 /* eslint-disable @typescript-eslint/no-empty-interface */
-import type { PublicKey } from "@solana/web3.js";
+import type { PublicKey } from '@solana/web3.js';
 
 interface CustomMatchers<R = unknown> {
-    /**
-     * Checks that a `PublicKey` is what you expect.
-     */
-    toEqualPubkey(expected: PublicKey): R;
-    /**
-     * Checks that specific `ProgramError` is thrown inside a function.
-     */
-    toThrowProgramError(code: number): R;
+  /**
+   * Checks that a `PublicKey` is what you expect.
+   */
+  toEqualPubkey(expected: PublicKey): R;
+  /**
+   * Checks that specific `ProgramError` is thrown inside a function.
+   */
+  toThrowProgramError(code: number): R;
+}
+
+declare global {
+  namespace jest {
+    interface Expect extends CustomMatchers {}
+    interface Matchers<R> extends CustomMatchers<R> {}
+    interface InverseAsymmetricMatchers extends CustomMatchers {}
   }
-  
-  declare global {
-    namespace jest {
-      interface Expect extends CustomMatchers {}
-      interface Matchers<R> extends CustomMatchers<R> {}
-      interface InverseAsymmetricMatchers extends CustomMatchers {}
-    }
-  }
+}
