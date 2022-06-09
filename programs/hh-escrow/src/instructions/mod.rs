e8cad580269e428a46bc34573011c3644db02a58
gyeiv <78514824+gyeiv@users.noreply.github.com>
2022-06-08 21:31:42 -0700

    Adds the SetGlobalState instruction.

--- programs/hh-escrow/src/instructions/mod.rs
+++ programs/hh-escrow/src/instructions/mod.rs
@@ -2,6 +2,7 @@ pub mod deposit;
 pub mod initialize_global_state;
 pub mod initialize_market;
 pub mod initialize_user_position;
+pub mod set_global_state;
 pub mod update_state;
 pub mod withdraw;
 
@@ -9,5 +10,6 @@ pub use self::deposit::*;
 pub use self::initialize_global_state::*;
 pub use self::initialize_market::*;
 pub use self::initialize_user_position::*;
+pub use self::set_global_state::*;
 pub use self::update_state::*;
 pub use self::withdraw::*;
