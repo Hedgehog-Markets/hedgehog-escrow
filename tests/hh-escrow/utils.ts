3bc0a1f763c6f1197a0ff1879484ee320526e037
James Whaley <juicy66173@gmail.com>
2022-07-29 18:54:15 +0100

    Start work on deploy tests

--- tests/hh-escrow/utils.ts
+++ tests/hh-escrow/utils.ts
@@ -3,7 +3,13 @@ import type { IdlTypes, Program } from "@project-serum/anchor";
 
 import * as anchor from "@project-serum/anchor";
 
-import { parseErrorCodes } from "../utils";
+import { Keypair, PublicKey, SystemProgram } from "@solana/web3.js";
+
+import {
+  BPF_LOADER_UPGRADEABLE_PROGRAM_ID,
+  intoU64BN,
+  parseErrorCodes,
+} from "../utils";
 
 type EscrowTypes = IdlTypes<HhEscrow>;
 
@@ -16,6 +22,91 @@ export type Outcome = EscrowTypes["Outcome"];
 export const program: Program<HhEscrow> = anchor.workspace.HhEscrow;
 
 export const ErrorCode = parseErrorCodes(program.idl.errors);
+
+export const [programData] = PublicKey.findProgramAddressSync(
+  [program.programId.toBytes()],
+  BPF_LOADER_UPGRADEABLE_PROGRAM_ID,
+);
+
+export const globalState = (() => {
+  const [address] = PublicKey.findProgramAddressSync(
+    [Buffer.from("global")],
+    program.programId,
+  );
+
+  const protocolFeeBps = 100; // 1%.
+
+  let _feeWallet = Keypair.generate();
+  let _authority = Keypair.generate();
+
+  return {
+    get address(): PublicKey {
+      return address;
+    },
+
+    get authority(): Keypair {
+      return _authority;
+    },
+    get feeWallet(): Keypair {
+      return _feeWallet;
+    },
+    get protocolFeeBps(): number {
+      return protocolFeeBps;
+    },
+
+    async initialize(): Promise<void> {
+      const state = await program.account.globalState.fetchNullable(address);
+      if (state) {
+        if (!state.authority.equals(_authority.publicKey)) {
+          throw new Error(
+            `Global state authority is not the expected: ${_authority.publicKey.toBase58()}`,
+          );
+        }
+        if (!state.feeWallet.equals(_feeWallet.publicKey)) {
+          throw new Error(
+            `Global state fee wallet is not the expected: ${_feeWallet.publicKey.toBase58()}`,
+          );
+        }
+        return;
+      }
+
+      await program.methods
+        .initializeGlobalState({
+          authority: _authority.publicKey,
+          feeWallet: _feeWallet.publicKey,
+          protocolFeeBps,
+        })
+        .accounts({
+          globalState: address,
+          payer: program.provider.wallet.publicKey,
+          upgradeAuthority: program.provider.wallet.publicKey,
+          escrowProgram: program.programId,
+          programData,
+          systemProgram: SystemProgram.programId,
+        })
+        .rpc();
+    },
+
+    async updateFeeWallet(feeWallet: Keypair): Promise<void> {
+      const state = await program.account.globalState.fetch(address);
+      if (!state.feeWallet.equals(feeWallet.publicKey)) {
+        throw new Error(
+          `Failed to update global state fee wallet to ${feeWallet.publicKey.toBase58()}, expected ${state.feeWallet.toBase58()}`,
+        );
+      }
+      _feeWallet = feeWallet;
+    },
+    async updateAuthority(authority: Keypair): Promise<void> {
+      const state = await program.account.globalState.fetch(address);
+      if (!state.authority.equals(authority.publicKey)) {
+        throw new Error(
+          `Failed to update global state authority to ${authority.publicKey.toBase58()}, expected ${state.authority.toBase58()}`,
+        );
+      }
+      _authority = authority;
+    },
+  };
+})();
 
 /**
  * Interpret the on-chain representation of a `UriResource` as a string.
